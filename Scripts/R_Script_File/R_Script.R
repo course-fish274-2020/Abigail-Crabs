79e034f784200f26b200ae09e4b0df6a9ec5c13f
ahuber3 <74076345+ahuber3@users.noreply.github.com>
2020-11-19 10:34:32 -0800

    Filtering data for specific values

--- Scripts/R_Script_File/R_Script.R
+++ Scripts/R_Script_File/R_Script.R
@@ -1,4 +1,3 @@
-# Part 1 ----
 library(ggplot2)
 library(dplyr)
 library(tidyverse)
@@ -6,26 +5,20 @@ library(tidyverse)
 burrow_dimensions_raw <- read.csv("Data/Ghost_crab_burrow_architecture.csv")
 head(burrow_dimensions_raw)
 
-# Part II: Filtering Data ----
+# Part I: Filtering Data ----
 # Filtering depth, temp, month for specific sites
       # Debidue Island + Folly Beach + Pawleys Island 1 +  Edisto Beach
 burrow_site_depth_temp_month <- burrow_dimensions_raw %>%
   select(site, burrow.depth, burrow.temperature, month) %>%
-  filter(site == "Debidue Island" | site == "Folly Beach" | site == "Pawleys Island.1" | site == "Edisto Beach")
+  filter(site == "Debidue Island" | site == "Folly Beach " | site == "Pawleys Island 1" | site == "Edisto Beach")
 head(burrow_site_depth_temp_month)
 
 
-
-
-
-
-
-
-
-
-
-gplot(burrow_dimensions_raw, aes(x = burrow.depth, color = site)) +
-  geom_histogram()
+ggplot(burrow_site_depth_temp_month, aes(x = burrow.depth, y = burrow.temperature,
+  color = site)) +
+  geom_point() +
+  facet_wrap(~site) +
+  geom_smooth(method = "loess")
 
 
 
