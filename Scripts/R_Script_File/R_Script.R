3e5cce68c6aeb8856cf9500293a2f4707a85315c
ahuber3 <74076345+ahuber3@users.noreply.github.com>
2020-11-19 12:58:11 -0800

    Plotting data

--- Scripts/R_Script_File/R_Script.R
+++ Scripts/R_Script_File/R_Script.R
@@ -1,5 +1,5 @@
+# Load libraries
 library(ggplot2)
-library(dplyr)
 library(tidyverse)
 #Reading in data
 burrow_dimensions_raw <- read.csv("Data/Ghost_crab_burrow_architecture.csv")
@@ -13,16 +13,16 @@ burrow_site_depth_temp_month <- burrow_dimensions_raw %>%
   filter(site == "Debidue Island" | site == "Folly Beach " | site == "Pawleys Island 1" | site == "Edisto Beach")
 head(burrow_site_depth_temp_month)
 
-
-ggplot(burrow_site_depth_temp_month, aes(x = burrow.depth, y = burrow.temperature,
-  color = site)) +
+#Plotting data
+burrow_data_plot <- ggplot(burrow_site_depth_temp_month, aes(x = burrow.depth, y = burrow.temperature,
+                                                             color = month)) +
   geom_point() +
   facet_wrap(~site) +
-  geom_smooth(method = "loess")
-
-
-
-
+  geom_smooth(method = "loess") +
+  labs(x = "Burrow Depth (mm)", y = "Burrow Temperature (°C)") +
+  labs(title = "Burrow Depth vs Temperature") +
+  theme(plot.title = element_text(hjust = 0.5))
+print(burrow_data_plot)
 
 
 
