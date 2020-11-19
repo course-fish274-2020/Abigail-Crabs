54a48ca9ea9db54bed9434190be395ab07c51d2a
ahuber3 <74076345+ahuber3@users.noreply.github.com>
2020-11-19 10:01:38 -0800

    Code

--- Scripts/R_Script_File/R_Script.R
+++ Scripts/R_Script_File/R_Script.R
@@ -7,10 +7,12 @@ burrow_dimensions_raw <- read.csv("Data/Ghost_crab_burrow_architecture.csv")
 head(burrow_dimensions_raw)
 
 # Part II: Filtering Data ----
-#Filtering site, depth, temp, month
+# Filtering depth, temp, month for specific sites
+      # Debidue Island + Folly Beach + Pawleys Island 1 +  Edisto Beach
 burrow_site_depth_temp_month <- burrow_dimensions_raw %>%
-  select(site, burrow.depth, burrow.temperature, month)
-# Debidue Island + Folly Beach + Pawleys Island 1 +  Edisto Beach
+  select(site, burrow.depth, burrow.temperature, month) %>%
+  filter(site == "Debidue Island" | site == "Folly Beach" | site == "Pawleys Island.1" | site == "Edisto Beach")
+head(burrow_site_depth_temp_month)
 
 
 
@@ -22,7 +24,7 @@ burrow_site_depth_temp_month <- burrow_dimensions_raw %>%
 
 
 
-ggplot(burrow_dimensions_raw, aes(x = burrow.depth, color = site)) +
+gplot(burrow_dimensions_raw, aes(x = burrow.depth, color = site)) +
   geom_histogram()
 
 
