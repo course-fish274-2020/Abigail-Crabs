78c7246bd232e48f2105a547bb748debad396c8b
ahuber3 <74076345+ahuber3@users.noreply.github.com>
2020-11-20 15:39:59 -0800

    Adding second plot to data and tidying up HTML file

--- Scripts/R_Script_File/R_Script.R
+++ Scripts/R_Script_File/R_Script.R
@@ -1,21 +1,19 @@
-# Load libraries
+# Load libraries ----
 library(ggplot2)
 library(tidyverse)
 #Reading in data
 burrow_dimensions_raw <- read.csv("Data/Ghost_crab_burrow_architecture.csv")
 head(burrow_dimensions_raw)
 
-# Part I: Filtering Data ----
-# Filtering depth, temp, month for specific sites
+# Filtering depth, temp, month for specific sites ----
       # Debidue Island + Folly Beach + Pawleys Island 1 +  Edisto Beach
 burrow_site_depth_temp_month <- burrow_dimensions_raw %>%
   select(site, burrow.depth, burrow.temperature, month) %>%
   filter(site == "Debidue Island" | site == "Folly Beach " | site == "Pawleys Island 1" | site == "Edisto Beach")
 head(burrow_site_depth_temp_month)
 
-#Plotting data
-burrow_data_plot <- ggplot(burrow_site_depth_temp_month, aes(x = burrow.depth, y = burrow.temperature,
-                                                             color = month)) +
+#Plotting data ----
+burrow_data_plot <- ggplot(burrow_site_depth_temp_month, aes(x = burrow.depth, y = burrow.temperature, color = month)) +
   geom_point() +
   facet_wrap(~site) +
   geom_smooth(method = "loess") +
@@ -25,8 +23,22 @@ burrow_data_plot <- ggplot(burrow_site_depth_temp_month, aes(x = burrow.depth, y
 print(burrow_data_plot)
 
 
+# Filtering data for another analysis
+    # Burrow shape vs burrow temp  for specific sites
+burrow_shape_temperature_sites <- burrow_dimensions_raw %>%
+  select(site, burrow.shape, burrow.temperature, month) %>%
+  filter(site == "Debidue Island" | site == "Folly Beach " | site == "Pawleys Island 1" | site == "Edisto Beach")
+head(burrow_shape_temperature_sites)
 
 
 
 
-
+#Plotting analysis 2
+burrow_data_plot_2 <- ggplot(burrow_shape_temperature_sites, aes(x = burrow.shape, y = burrow.temperature, color = burrow.shape)) +
+  geom_point() +
+  geom_jitter() +
+  facet_wrap(~site)+
+  labs(x = "Burrow Site", y = "Burrow Temperature (°C)") +
+  labs(title = "Burrow Shape vs Temperature") +
+  theme(plot.title = element_text(hjust = 0.5))
+print(burrow_data_plot_2)
