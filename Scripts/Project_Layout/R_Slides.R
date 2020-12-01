b708f3e8182f4916022d7895688e1f570e27d84d
ahuber3 <74076345+ahuber3@users.noreply.github.com>
2020-12-01 13:15:24 -0800

    Creating project layout, adding Ghost Crab Background slide

---
+++ Scripts/Project_Layout/R_Slides.R
@@ -0,0 +1,59 @@
+
+# Concept ----
+  # What are Ghost crabs?
+  # Where do they live?
+  # Where is data from?
+
+
+# Goals ----
+  # Present code in way that is understandable
+  # Find out if burrow shape and/or depth affect burrow humidity and/or temp
+
+
+# Required packages
+
+
+
+# 1. Import data ----
+  # Where is data from? (year 2016)
+  # Insert link
+  # Head data (change width)
+
+
+
+# 2. Filter data ----
+  # Minimizes extra variables
+      # 1 Site
+      # 1 month
+      # Shape, depth, temperature, humidity
+
+
+
+# 3. How does shape affect humidity and temp of burrow? ----
+  #Select shape and temp
+    # (a). Shape vs. temperature
+        # Table
+        # Graph
+    # (b). Shape vs. humidity
+        #Table
+        #Graph
+    # (c). Is there an effect?
+
+
+# 4. How does depth affect humidity and temp of burrow? ----
+  #Select depth and humidity
+    # (a). Depth vs. temp
+        # Table
+        # Graph
+    # (b). Depth vs. Humidity
+        # Table
+        # Graph
+    # (c). Is there an effect?
+
+
+# Conclusion ----
+  # Re-show graphs
+  # Restate if correlation apparent
+  # Thank you slide
+
+
